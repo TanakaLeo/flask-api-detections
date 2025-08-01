5f1377a9ddc63a525942a04c73e71b9fa22ea39b
Tanaka Leo <112909170+TanakaLeo@users.noreply.github.com>
2025-08-01 00:21:22 -0300

    Corrige API para uso no Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -10,10 +11,13 @@ def index():
 @app.route('/detections', methods=['POST'])
 def receber_dados():
     data = request.get_json()
-    frame = data.get('frame')
     objetos = data.get('objetos')
     tempo = data.get('tempo_ms')
     timestamp = datetime.now().isoformat()
 
-    print(f"[{timestamp}] Frame {frame}: {objetos} ({tempo}ms)")
-    return jsonify({"status": "recebido", "frame": frame}), 200
+    print(f"[{timestamp}] Objetos: {objetos} ({tempo}ms)")
+    return jsonify({"status": "recebido"}), 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
